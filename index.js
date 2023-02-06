0a5a7af64b516d36ce333f85b7401b40775176ff
vedVika teach <vedvika@vedVikas-MacBook-Air.local>
2023-02-06 15:30:54 +0530

    some update new update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // import express from "express";
-// import cors from "cors";
-// import * as dotenv from "dotenv";
-// import Jimp from "jimp";
-// import { converBase64ToImage } from "convert-base64-to-image";
-// import Tesseract, { createWorker } from "tesseract.js";
-// import resemble from "node-resemble-js";
-// import tesseract from "node-tesseract-ocr";
-// // import resemble from "resemblejs";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import Jimp from "jimp";
+import { converBase64ToImage } from "convert-base64-to-image";
+import Tesseract, { createWorker } from "tesseract.js";
+import resemble from "node-resemble-js";
+import tesseract from "node-tesseract-ocr";
+// import resemble from "resemblejs";
 
 // dotenv.config();
 
@@ -173,6 +173,118 @@ app.listen(PORT, () => {
 
 app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
+});
+app.post("/", (req, res) => {
+  if (req.body.img !== null) {
+          const base64 = req.body?.img;
+          const pathToSaveImage = "./image.png";
+          converBase64ToImage(base64, pathToSaveImage);
+    
+          const config = {
+            lang: "eng",
+            oem: 1,
+            psm: 3,
+          };
+    
+          // var diffi = await resemble("./image.png")
+          //   .compareTo("./A.png")
+          //   .ignoreColors()
+          //   .onComplete(function (dt) {
+          //     console.log(100 - dt.misMatchPercentage);
+          //     // console.log(dt);
+          //   });
+    
+          //   async function compareImages(image1Url, image2Url) {
+    
+          //     const image1 = await Jimp.read(image1Url);
+          //     const image2 = await Jimp.read(image2Url);
+          //     // Perceived distance
+          //     const distance = Jimp.distance(image1, image2);
+          //     // Pixel difference
+          //     const diff = Jimp.diff(image1, image2);
+    
+          //     console.log(`compareImages: distance: ${distance.toFixed(3)}, diff.percent: ${diff.percent.toFixed(3)}`);
+          //     if (distance < 0.15 || diff.percent < 0.15) {
+          //         console.log("compareImages: Images match!");
+          //         return true;
+          //     } else {
+          //         console.log("compareImages: Images do NOT match!");
+          //         return false;
+          //     }
+          // }
+    
+          // const usFlag = "https://flaglane.com/download/american-flag/american-flag-small.jpg";
+          // const canadianFlagJpg = "https://flaglane.com/download/canadian-flag/canadian-flag-small.jpg";
+          // const canadianFlagPng = "https://flaglane.com/download/canadian-flag/canadian-flag-small.png";
+    
+          // These should not match.
+          // compareImages(usFlag, canadianFlagJpg);
+    
+          // // These should match.
+          // compareImages(canadianFlagJpg, canadianFlagPng);
+    
+          const example1 = await Jimp.read(`./Alphabet/${req.body?.title}(1).png`);
+          // console.log(example1)
+          const example2 = await Jimp.read("./image.png");
+          // const testing = example1.blit(example2);
+          // console.log(testing);
+          const check1 = example1.resize(300, Jimp.AUTO);
+          const check2 = example2.resize(300, Jimp.AUTO);
+          const distance = Jimp.distance(example1, example2);
+          const diff = Jimp.diff(check1, check2);
+          const result = diff.percent.toFixed(3) * 1000;
+          // console.log(
+          //   `compareImages: distance: ${distance.toFixed(3)}, diff.percent: ${
+          //     diff.percent.toFixed(3) * 1000
+          //   }`
+          // );
+    
+          // Tesseract.recognize("./image.png")
+          //   .setOptions({
+          //     tessedit_char_whitelist:
+          //       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+          //   })
+          //   .then(function (result) {
+          //     console.log(result.text);
+          //   });
+          // Tesseract.recognize("./image.png", {
+          //   tessedit_char_whitelist:
+          //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+          // })
+          //   .then(function (result) {
+          //     console.log(result.text);
+          //   })
+          //   .catch(function (err) {
+          //     console.error(err);
+          //   });
+    
+          const txt = await Tesseract.recognize("./image.png", "eng", {
+            logger: (m) => m,
+          }).then(({ data: { text } }) => {
+            console.log(text);
+            return text;
+          });
+          // console.log(txt);
+          res.status(200).send({
+            text: txt,
+            result: 100 - result,
+            // result: 100 - result * 100,
+          });
+    
+          // const txt = await Tesseract.recognize(`./${req.body?.title}.png`, "eng", {
+          //   logger: (m) => m,
+          // }).then(({ data: { text } }) => {
+          //   console.log(text);
+          // });
+    
+          // res.status(200).send({
+          //   // text: txt,
+          //   result: 100 - result,
+          //   // result: 100 - result * 100,
+          // });
+        } else {
+          console.log("some think error");
+        }
 });
 
 app.get("/about", (req, res) => {
